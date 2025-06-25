ed2afdae85784070e79e9fce61664613a39ab00a
Abdulla <abduqulovabdulla3108@gmail.com>
2025-06-25 20:52:18 +0500

    fixed static

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.views.static import serve
 from drf_yasg import openapi
 from drf_yasg.generators import OpenAPISchemaGenerator
 from drf_yasg.views import get_schema_view
@@ -37,6 +38,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("default-admin-panel/", admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += [
